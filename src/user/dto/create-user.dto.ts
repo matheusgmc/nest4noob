2bf79c1d072ae64839865f4641427323ea976a10
matheusgmc <sdmastergames@hotmail.com>
2022-10-12 11:12:27 -0400

    adding prop password for user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,8 @@
-import { IsEmail } from 'class-validator';
-import { Prisma } from "@prisma/client";
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Prisma } from '@prisma/client';
 
 export class CreateUserDTO implements Prisma.UserCreateInput {
   @IsEmail() readonly email: string;
+  @IsNotEmpty() readonly password: string;
   readonly name?: string | null | undefined;
 }
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -5,18 +5,20 @@ import { User } from '@prisma/client';
 
 const mockUser: User[] = [
   {
-    email: "test@test.com",
-    name: "test1",
-    id: 1
+    email: 'test@test.com',
+    name: 'test1',
+    id: 1,
+    password: 'senha123',
   },
   {
-    email: "example@example.com",
-    name: "test2",
-    id: 2
-  }
-]
+    email: 'example@example.com',
+    name: 'test2',
+    id: 2,
+    password: 'senha123',
+  },
+];
 
-describe("UserService", () => {
+describe('UserService', () => {
   let suit: UserService;
 
   beforeAll(async () => {
@@ -27,101 +29,113 @@ describe("UserService", () => {
           provide: PrismaService,
           useValue: {
             user: {
-              findUnique: jest.fn().mockResolvedValue(mockUser[0]).mockResolvedValueOnce(mockUser[0]).mockResolvedValueOnce(null),
+              findUnique: jest
+                .fn()
+                .mockResolvedValue(mockUser[0])
+                .mockResolvedValueOnce(mockUser[0])
+                .mockResolvedValueOnce(null),
               findMany: jest.fn().mockResolvedValue(mockUser),
-              create: jest.fn().mockResolvedValue(
-                {
-                  email: "user@user.com",
-                  name: "user test",
-                  id: 3
-                }
-              ),
+              create: jest.fn().mockResolvedValue({
+                email: 'user@user.com',
+                name: 'user test',
+                id: 3,
+              }),
               update: jest.fn().mockResolvedValueOnce({
                 ...mockUser[0],
-                name: "test user"
+                name: 'test user',
               }),
-              delete: jest.fn().mockResolvedValueOnce(mockUser[0])
-            }
-          }
-        }
-      ]
+              delete: jest.fn().mockResolvedValueOnce(mockUser[0]),
+            },
+          },
+        },
+      ],
     }).compile();
 
     suit = moduleRef.get<UserService>(UserService);
-
   });
 
-  it("should be defined", () => {
+  it('should be defined', () => {
     expect(suit).toBeDefined();
   });
 
-  describe("users", () => {
-    it("should return a list of all users", async () => {
+  describe('users', () => {
+    it('should return a list of all users', async () => {
       expect(await suit.users({})).toEqual(mockUser);
     });
   });
 
-  describe("user", () => {
-    it("should return a user by id", async () => {
-      await expect(suit.user({
-        id: 1
-      })).resolves.toEqual(mockUser[0]);
+  describe('user', () => {
+    it('should return a user by id', async () => {
+      await expect(
+        suit.user({
+          id: 1,
+        }),
+      ).resolves.toEqual(mockUser[0]);
     });
 
-    it("should have an exception when the user is not found", async () => {
-      await expect(suit.user({
-        id: 10
-      })).rejects.toThrowError("user not found");
+    it('should have an exception when the user is not found', async () => {
+      await expect(
+        suit.user({
+          id: 10,
+        }),
+      ).rejects.toThrowError('user not found');
     });
   });
 
-  describe("create user", () => {
-    it("should create a user with success", async () => {
-      await expect(suit.createUser({
-        email: "user@user.com",
-        name: "user test"
-      })).resolves.toEqual(
-        {
-          email: "user@user.com",
-          name: "user test",
-          id: 3
-        }
-      );
-    });
-
-    it("should return an exception in the absence of a param", async () => {
-      await expect(suit.createUser({
-        email: "",
-        name: "user test"
-      })).rejects.toHaveProperty("response.message", ["email must be an email"]);
-    });
-
-  });
-
-  describe("update user", () => {
-    it("should update name successfully", async () => {
-      await expect(suit.updateUser({
-        data: {
-          name: "test user"
-        },
-        where: {
-          id: 1
-        }
-      })).resolves.toEqual({
-        ...mockUser[0],
-        name: "test user"
+  describe('create user', () => {
+    it('should create a user with success', async () => {
+      await expect(
+        suit.createUser({
+          email: 'user@user.com',
+          name: 'user test',
+          password: 'senha123',
+        }),
+      ).resolves.toEqual({
+        email: 'user@user.com',
+        name: 'user test',
+        id: 3,
       });
-
     });
 
+    it('should return an exception in the absence of a param', async () => {
+      await expect(
+        suit.createUser({
+          email: '',
+          name: 'user test',
+          password: '',
+        }),
+      ).rejects.toHaveProperty('response.message', [
+        'email must be an email',
+        'password should not be empty',
+      ]);
+    });
   });
 
-  describe("delete user", () => {
-    it("should delete user successfully", async () => {
-      await expect(suit.deleteUser({
-        id: 1
-      })).resolves.toEqual(mockUser[0])
+  describe('update user', () => {
+    it('should update name successfully', async () => {
+      await expect(
+        suit.updateUser({
+          data: {
+            name: 'test user',
+          },
+          where: {
+            id: 1,
+          },
+        }),
+      ).resolves.toEqual({
+        ...mockUser[0],
+        name: 'test user',
+      });
+    });
+  });
 
+  describe('delete user', () => {
+    it('should delete user successfully', async () => {
+      await expect(
+        suit.deleteUser({
+          id: 1,
+        }),
+      ).resolves.toEqual(mockUser[0]);
     });
   });
 });
