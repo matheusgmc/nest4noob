0e49cb3257e32df9945f0d411e25d7854bb3d455
matheusgmc <sdmastergames@hotmail.com>
2022-10-15 02:30:55 -0400

    adding RootController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { RootController } from './root/root.controller';
 
 @Module({
   imports: [UserModule, PostModule],
+  controllers: [RootController],
 })
 export class AppModule { }
