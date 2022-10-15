1449e7cac96c29c54c647617c0140416d29c5703
matheusgmc <sdmastergames@hotmail.com>
2022-10-15 02:33:14 -0400

    adding method for cleanup

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,16 +1,23 @@
-import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common"
-import { PrismaClient } from "@prisma/client";
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    this.$on("beforeExit", async () => {
+    this.$on('beforeExit', async () => {
       await app.close();
-    })
+    });
+  }
+
+  //used only in tests
+  async cleanup() {
+    const deleteUser = this.user.deleteMany();
+    const deletePost = this.post.deleteMany();
+
+    await this.$transaction([deleteUser, deletePost]);
   }
 }
