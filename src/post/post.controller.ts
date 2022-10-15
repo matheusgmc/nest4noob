d387ab0e4405a394887910a493a3c80c507b8bb5
matheusgmc <sdmastergames@hotmail.com>
2022-10-15 02:34:31 -0400

    formatting

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,81 +1,102 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
-import { Post as PostModel } from "@prisma/client";
-import { UserService } from 'src/user/user.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { Post as PostModel } from '@prisma/client';
+import { UserService } from '../user/user.service';
 import { CreatePostDTO } from './dto/create-post.dto';
 import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
-  constructor(private postService: PostService, private userService: UserService) { }
+  constructor(
+    private postService: PostService,
+    private userService: UserService,
+  ) { }
 
-  @Get("feed/")
-  async getPublishedPosts(@Query("search") search: string): Promise<PostModel[]> {
+  @Get('feed/')
+  async getPublishedPosts(
+    @Query('search') search: string,
+  ): Promise<PostModel[]> {
     return this.postService.posts({
       where: {
         published: true,
         OR: [
           {
-            title: { contains: search }
+            title: { contains: search },
           },
           {
-            content: { contains: search }
-          }
-        ]
-      }
-    })
-  }
-
-
-  @Get(":id")
-  async getPostById(@Param("id", new ParseIntPipe()) id: number): Promise<PostModel | null> {
-    return this.postService.post({
-      id: Number(id)
+            content: { contains: search },
+          },
+        ],
+      },
     });
   }
 
+  @Get(':id')
+  async getPostById(
+    @Param('id', new ParseIntPipe()) id: number,
+  ): Promise<PostModel | null> {
+    return this.postService.post({
+      id: Number(id),
+    });
+  }
 
   @Post()
   async createDraft(@Body() postData: CreatePostDTO): Promise<PostModel> {
     const { title, content, authorEmail } = postData;
-    if (await this.userService.users({
-      where: {
-        email: authorEmail
-      }
-    })) {
-      throw new HttpException({
-        statusCode: HttpStatus.NOT_FOUND,
-        message: "user not found",
-        error: "NotFound",
-      }, HttpStatus.NOT_FOUND);
+    if (
+      await this.userService.users({
+        where: {
+          email: authorEmail,
+        },
+      })
+    ) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: 'user not found',
+          error: 'NotFound',
+        },
+        HttpStatus.NOT_FOUND,
+      );
     }
     return this.postService.createPost({
       title,
       content,
       author: {
         connect: {
-          email: authorEmail
-        }
-      }
-    })
+          email: authorEmail,
+        },
+      },
+    });
   }
 
-
-  @Put("publish/:id")
-  async publishPost(@Param("id") id: string): Promise<PostModel> {
+  @Put('publish/:id')
+  async publishPost(@Param('id') id: string): Promise<PostModel> {
     return this.postService.updatePost({
       where: {
-        id: Number(id)
+        id: Number(id),
       },
       data: {
-        published: true
-      }
-    })
+        published: true,
+      },
+    });
   }
 
-  @Delete(":id")
-  async deletePost(@Param("id") id: string): Promise<PostModel> {
+  @Delete(':id')
+  async deletePost(@Param('id') id: string): Promise<PostModel> {
     return this.postService.deletePost({
-      id: Number(id)
-    })
+      id: Number(id),
+    });
   }
 }
