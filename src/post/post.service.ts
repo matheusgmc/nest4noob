7dcfef50f3813bb41e43323d48f5400276c57fa1
matheusgmc <sdmastergames@hotmail.com>
2022-10-15 02:35:51 -0400

    formatting

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Post, Prisma } from '@prisma/client'
-import { PrismaService } from 'src/database/prisma.service';
+import { Post, Prisma } from '@prisma/client';
+import { PrismaService } from '../database/prisma.service';
 @Injectable()
 export class PostService {
   constructor(private prisma: PrismaService) { }
 
-  async post(postWhereUniqueInput: Prisma.PostWhereUniqueInput): Promise<Post | null> {
+  async post(
+    postWhereUniqueInput: Prisma.PostWhereUniqueInput,
+  ): Promise<Post | null> {
     return this.prisma.post.findUnique({
-      where: postWhereUniqueInput
+      where: postWhereUniqueInput,
     });
   }
 
@@ -25,7 +27,10 @@ export class PostService {
     return this.prisma.post.create({ data });
   }
 
-  async updatePost(params: { where: Prisma.PostWhereUniqueInput, data: Prisma.PostUpdateInput }): Promise<Post> {
+  async updatePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+    data: Prisma.PostUpdateInput;
+  }): Promise<Post> {
     return this.prisma.post.update(params);
   }
 
