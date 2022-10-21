47873618013a00b5f02de8c368940e7b6485da1e
matheusgmc <sdmastergames@hotmail.com>
2022-10-21 01:18:58 -0400

    add auth service in providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
 import { PrismaService } from '../database/prisma.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PrismaService, UserService],
+  providers: [PrismaService, UserService, AuthService],
 })
-export class UserModule { }
+export class UserModule {}
